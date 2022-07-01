942b28e8dd73599b09f9bb88258abfaac5acb5fc
Mateusz Słomka <slomkam7@gmail.com>
2022-07-01 13:57:02 +0200

    initial commit

---
+++ data/hades/functions/start.mcfunction
@@ -0,0 +1,1 @@
+tellraw @a {"text": "The hades datapack is enabled!"}
---
+++ pack.js
@@ -0,0 +1,42 @@
+import fs from 'fs/promises';
+import FS from 'fs';
+import JsZip from 'jszip';
+
+const zip = new JsZip();
+
+(async () => {
+  try {
+    const mcMeta = await fs.readFile('./pack.mcmeta');
+    zip.file('pack.mcmeta', mcMeta);
+
+    /**
+     * @param {string[]} paths
+     */
+    async function read( paths ) {
+      for (const path of paths) {
+        if (path.includes('.')) {
+          const data = await fs.readFile(path);
+          zip.file( path, data );
+          continue;
+        }
+
+        console.log(path);
+        const c = await fs.readdir(path);
+        const dirPath = path;
+        zip.folder(dirPath);
+        await read([ ...c.map( c => path + '/' + c ) ]);
+      }
+    }
+
+    await read([ 'data' ]);
+
+    zip.generateNodeStream({ type: 'nodebuffer', streamFiles: true })
+      .pipe(FS.createWriteStream('deep-dark-expansion-v1.zip'))
+      .on('finish', function () {
+          console.log("zip file written");
+      });
+    
+  } catch(err) {
+    console.warn(err);
+  }
+})();
